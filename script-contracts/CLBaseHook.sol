6a1f658a4efbdb428149c260238dc9fbf261140a
Dev Katyal <devkatyal01@gmail.com>
2025-05-31 15:32:09 +0200

    PoolManager + vaultManager functios

--- script-contracts/CLBaseHook.sol
+++ script-contracts/CLBaseHook.sol
@@ -25,3 +25,54 @@ import {IVault} from "@pancakeswap/v4-core/src/interfaces/IVault.sol";
 import {ICLHooks} from "@pancakeswap/v4-core/src/pool-cl/interfaces/ICLHooks.sol";
 import {ICLPoolManager} from "@pancakeswap/v4-core/src/pool-cl/interfaces/ICLPoolManager.sol";
 import {CLPoolManager} from "@pancakeswap/v4-core/src/pool-cl/CLPoolManager.sol";
+
+
+abstract contract CLBaseHook is ICLHooks {
+    error NotPoolManager();
+    error NotVault();
+    error NotSelf();
+    error InvalidPool();
+    error LockFailure();
+    error HookNotImplemented();
+
+    struct Permissions {
+        bool beforeInitialize;
+        bool afterInitialize;
+        bool beforeAddLiquidity;
+        bool afterAddLiquidity;
+        bool beforeRemoveLiquidity;
+        bool afterRemoveLiquidity;
+        bool beforeSwap;
+        bool afterSwap;
+        bool beforeDonate;
+        bool afterDonate;
+        bool beforeSwapReturnsDelta;
+        bool afterSwapReturnsDelta;
+        bool afterAddLiquidityReturnsDelta;
+        bool afterRemoveLiquidityReturnsDelta;
+    }
+
+    /// @notice The address of the pool manager
+    ICLPoolManager public immutable poolManager;
+
+    /// @notice The address of the vault
+    IVault public immutable vault;
+
+    constructor(ICLPoolManager _poolManager) {
+        poolManager = _poolManager;
+        vault = CLPoolManager(address(poolManager)).vault();
+    }
+
+    /// @dev Only the pool manager may call this function
+    modifier poolManagerOnly() {
+        if (msg.sender != address(poolManager)) revert NotPoolManager();
+        _;
+    }
+
+    /// @dev Only the vault may call this function
+    modifier vaultOnly() {
+        if (msg.sender != address(vault)) revert NotVault();
+        _;
+    }
+
+}
